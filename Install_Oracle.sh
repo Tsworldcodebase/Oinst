cd69693a3a74d39104a500aaa7794b00ead8cdd9
Tyler Jakubowski <96379616+Tsworldcodebase@users.noreply.github.com>
2023-07-03 09:42:39 -0400

    Update Install_Oracle.sh

--- Install_Oracle.sh
+++ Install_Oracle.sh
@@ -2,6 +2,6 @@
 #!/bin/bash
 subscription-manager register --username [your username without brackets here]--password [your Password goes here without these brackets]
 yum -y update & wait $! 
-yum -y upgrade
-yum install -y oracle-database-preinstall-19c
+yum -y upgrade & wait $!
+yum -y install https://yum.oracle.com/repo/OracleLinux/OL8/appstream/x86_64/getPackage/oracle-database-preinstall-19c-1.0-2.el8.x86_64.rpm 
 
