26bca32322dbfd565bc747b22d9a31be3a3224ca
Tyler Jakubowski <96379616+Tsworldcodebase@users.noreply.github.com>
2023-07-08 08:40:21 -0400

    Update Install_Oracle.sh

--- Install_Oracle.sh
+++ Install_Oracle.sh
@@ -1,11 +1,10 @@
 # This script installs all Oracle componets and uses silent install file for DB #setup. You need a RHEL subscription and RHEL 8.2 OS.
 # Oracle rpm provided must be uploaded to /tmp/oracle directory ( create this directory before uploading file) check file name and script too.
 #!/bin/bash
-subscription-manager register --username [your username without brackets here]--password [your Password goes here without these brackets]
-yum -y update & wait $! 
+subscription-manager register --username []--password []
+yum -y update & wait $!
 yum -y upgrade & wait $!
-yum -y install https://yum.oracle.com/repo/OracleLinux/OL8/appstream/x86_64/getPackage/oracle-database-preinstall-19c-1.0-2.el8.x86_64.rpm
-yum -y yum localinstall /tmp/oracle/oracle-database-ee-19c-1.0-1.x86_64\(1\).rpm
-yum install -y https://downloads.druva.com/downloads/Phoenix/Linux/druva-phoenix-oracle-client-6.1.1-328698.x86_64.rpm
-
-/etc/init.d/oracledb_ORCLCDB-19c configure
+yum -y install https://yum.oracle.com/repo/OracleLinux/OL8/appstream/x86_64/getPackage/oracle-database-preinstall-19c-1.0-2.el8.x86_64.rpm & wait $!
+yum localinstall -y /tmp/oracle/oracle-database-ee-19c-1.0-1.x86_64\(1\).rpm & wait $!
+yum install -y https://downloads.druva.com/downloads/Phoenix/Linux/druva-phoenix-oracle-client-6.1.1-328698.x86_64.rpm && /etc/init.d/oracledb_ORCLCDB-19c configure
+~                          
